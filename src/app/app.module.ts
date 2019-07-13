2fe25e3262b1bc18a86a50687322b3f8407ec4ea
Fortino Sandoval <fortino.sandoval@maritz.com>
2019-07-13 16:09:55 -0700

    created material module for material component imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { JwtInterceptor } from './helpers/jwt.interceptor';
 import { ErrorInterceptor } from './helpers/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
+import { MaterialModule } from './material.module';
 import { LogoutComponent } from './components/logout/logout.component';
 
 @NgModule({
@@ -30,11 +28,9 @@ import { LogoutComponent } from './components/logout/logout.component';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatCardModule,
-    MatInputModule
+    MaterialModule
   ],
-  exports: [MatButtonModule, MatCardModule, MatInputModule],
+  exports: [MaterialModule],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
