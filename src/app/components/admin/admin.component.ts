9851bb5f953ed714bcc531ca185c052c73df86ee
Fortino Sandoval <fortino.sandoval@maritz.com>
2019-07-13 16:10:19 -0700

    sidebar added

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from './../../services/auth.service';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.less']
 })
 export class AdminComponent implements OnInit {
-  
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private authService: AuthService) {}
+  sidenavOpen = false;
+  ngOnInit() {}
+  logout() {
+    this.authService.logout('/');
   }
-  
 }
